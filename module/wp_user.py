cc78bedd0ef73eba5e50643db13636b669c44b4f
Debby Anggraini <Xnver404@gmail.com>
2018-08-05 20:03:09 +0700

    Add files via upload

--- module/wp_user.py
+++ module/wp_user.py
@@ -5,7 +5,6 @@
 from core.misc import printf
 import urllib
 import urllib2
-import sys
 
 def uniq(lst):
     last = object()
@@ -44,10 +43,8 @@ def user_scan(url,usern):
             site = 'http://' + url
     else:
         site = url
-    printf('scanning (%s)' % site)
+    printf('wait a minute, scanning (%s)' % site)
     for x in range(0,int(usern)):
-        print('\rprogress %s'%(100/int(usern)*x)+'%'),
-        sys.stdout.flush()
         try:
             tmp = curllib(site, '', urllib.urlencode({"author":(x+1)}))
             if tmp == False:
@@ -58,27 +55,10 @@ def user_scan(url,usern):
             pass
         if len(tmp['user']):
             results.append(tmp)
-            max_login_len = len(tmp['user']) if max_login_len < len(tmp['user']) else max_login_len
-            max_name_len = len(tmp['name']) if max_name_len < len(tmp['name']) else max_name_len
     if not results:
         printf('\nCould not find anything, maybe not vulnerable!')
         return
     results = sort_and_deduplicate(results)
-    print("\nFound "+str( len( results ) )+" users in "+site+"")
-
-    login_space = (max_login_len-len("Login")+1)*" "
-    name_space = (max_name_len-len("Name")+1)*" "
-    login_bar = ((max_login_len-len("Login")+1)+6)*"-"
-    name_bar = ((max_name_len-len("Name")+1)+5)*"-"
-    header = "| Id | Login"+login_space+"| Name"+name_space+"|"
-
-    print("  +----+"+login_bar+"+"+name_bar+"+")
-    print("  "+header)
-    print("  +----+"+login_bar+"+"+name_bar+"+")
-
-    for x in range(0,len(results)):
-        id_space = (3-len(str(x+1)))*" "
-        login_space = (max_login_len-len(results[x]['user'])+1)*" "
-        name_space = (max_name_len-len(results[x]['name'])+1)*" "
-        print("  | "+str(x+1)+id_space+"| "+results[x]['user']+login_space+"| "+results[x]['name']+name_space+"|")
-    print("  +----+"+login_bar+"+"+name_bar+"+")
+    print("Found "+str( len( results ) )+" users in "+site+"")
+    for i in range(0,len(results)):
+         printf('\nuser: %s\nname: %s' % (results[i]['user'],results[i]['name']))

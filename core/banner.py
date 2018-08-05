37b028ad758864f98c325a9faf7e7d24c81cba8d
Debby Anggraini <Xnver404@gmail.com>
2018-08-05 13:25:31 +0700

    Add files via upload

---
+++ core/banner.py
@@ -0,0 +1,17 @@
+##! usr/bin/python2.7
+## coding: utf-8
+##
+## banner | 37 scanner • 17 encryption
+##
+## written by @ciku370
+##
+
+def banner():
+    print('''
+\t{0} ,------. {2}•{1} Web Security Testing {2}•
+\t{0} |  .---' ,--,--.,-----.,--. ,--.
+\t{0} |  `--, ' ,-.  |`-.  /  \  '  /
+\t{0} |  `---.\ '-'  | /  `-.  \   /
+\t{0} `------' `--`--'`-----'.-'  / {1}framework.
+\t{1}  127.0.0.1 {2}•{1} @CiKu370  {0}`---'
+'''.format('\033[94m','\033[0m','\033[91m'))

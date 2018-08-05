0edfcdf83264ff247b5d690b5c50f3ddf55a1df2
Debby Anggraini <Xnver404@gmail.com>
2018-08-05 13:32:09 +0700

    Add files via upload

---
+++ Eazy.py
@@ -0,0 +1,25 @@
+##
+##   ## ,------. ##########################
+##   ## |  .---' ,--,--.,-----.,--. ,--. ##
+##   ## |  `--, ' ,-.  |`-.  /  \  '  / ###
+##   ## |  `---.\ '-'  | /  `-.  \   ' ####
+##   ## `------' `--`--'`-----'.-'  / #####
+##   ########### framework ### `---' ######
+##
+##     make your fuckin' dream come true
+##
+## written by @ CiKu370
+##
+## follow my social media
+##
+## telegram: @CiKu370
+## facebook: https://facebook.com/putriy.kaeysha
+## github: https://github.com/ciku370
+##
+
+def main():
+    from core.shell import debby_anggraini as bintari
+    bintari()
+
+if __name__ == '__main__':
+    main()

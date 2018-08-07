cf2bd40bc02b5153d4bde4730bd9bacd9a3e5e2e
Debby Anggraini <Xnver404@gmail.com>
2018-08-07 09:01:57 +0700

    Add files via upload

--- core/misc.py
+++ core/misc.py
@@ -5,20 +5,20 @@
 ##
 
 def printf(msg,code = 0):
-    msg = msg.replace('?*','\033[95;1m*\033[0m').replace('?-','\033[94;1m-\033[0m').replace('?=>','\033[94;1m=>\033[0m').replace('{0}','\033[91m').replace('{1}','\033[0m')
+    msg = msg.replace('{0}','\033[91m').replace('{1}','\033[0m').replace('[+]','\033[94m[+]\033[0m').replace('[!]','\033[91m[!]\033[0m').replace('[?]','\033[94m[?]\033[0m')
     if code == 0:
         print('\033[0m%s\033[0m'%msg)
     elif code == 1:
         print("\033[0m[\033[92m%s\033[0m] %s"%(u(" %T "),msg))
     elif code == 2:
-        print("\033[0mError: %s"%msg)
+        print("\033[91m[!] \033[0mError: %s"%msg)
 
 def show_modules(t = None):
     if t == None or t == 'wibu bau bawang':
           printf("""{0}
- web scanner       description{1}
- -----------       -----------
- dork              dork scanner.
+ scanner           description{1}
+ -------           -----------
+ dork              dork scanner with Sqli testing.
  traceroute        traceroute using MRT.
  nping             test ping/Nping.
  dns_lookup        dns lookup.
@@ -38,16 +38,23 @@ def show_modules(t = None):
  whatcms           Check Content Management System (CMS).
  subdomain         subdomain scanner using AXFR technique.
  honeypot          honeypot detector.
- wpscan            wordpress plugins scanners.
  nmap              advanced nmap.
- user_pro          wordpress userpro vulnerability scanner.
  dirscan           directory scanner.
  shell             scan available shell on websites.
  hashbuster        online hash scanner/decrypter.
  adfin             search admin pages.
  upload            search upload pages.
- wp_user           Wordpress users enumerate
-                   bypass to get the website users.
+ wpscan            wordpress plugins scanners.
+ lfi_scan          scan Local File Inclusion vulnerabilities.
+ {0}
+ exploit{1}
+ =======
+ {0}
+   wordpress       description{1}
+   ---------       -----------
+   wp_sym_exp      Wordpress WP Symposium 14.11 Shell Upload Vulnerability.
+   user_pro        wordpress userpro vulnerability scanner.
+   wp_user         Wordpress users enumerate bypass to get the website users.
 """)
     if t == 'wibu bau bawang':
           printf("""{0} encryption        description{1}

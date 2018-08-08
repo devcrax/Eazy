8a81b9eefd20976d5def06ab277366f2b77c3381
Debby Anggraini <Xnver404@gmail.com>
2018-08-08 23:24:53 +0700

    Add files via upload

--- Eazy.py
+++ Eazy.py
@@ -17,7 +17,22 @@
 ## github: https://github.com/ciku370
 ##
 
+version = 1.19
+
+def check_update():
+    from core.misc import printf
+    import os
+    import requests
+    cek = requests.get('https://raw.githubusercontent.com/CiKu370/Eazy/master/Eazy.py')
+    for i in cek.text:
+         if 'version = ' in i:
+             k = i[10:]
+             if k != version:
+                 printf('[+] updating..')
+                 os.system('cd ~;rm -rf Eazy;git clone https://github.com/ciku370/Eazy')
+                 break
 def main():
+    check_update()
     from core.shell import debby_anggraini as bintari
     bintari()
 

